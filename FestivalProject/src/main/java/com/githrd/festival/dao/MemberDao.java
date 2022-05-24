db382380348fc01e9c910153ec85fe315ae2f839
tmddus2123 <tmddus2123@gmail.com>
2022-05-25 01:26:22 +0900

    회원가입 기능

--- FestivalProject/src/main/java/com/githrd/festival/dao/MemberDao.java
+++ FestivalProject/src/main/java/com/githrd/festival/dao/MemberDao.java
@@ -4,6 +4,7 @@ import java.sql.*;
 
 import com.githrd.festival.db.*;
 import com.githrd.festival.sql.*;
+import com.githrd.festival.vo.*;
 
 public class MemberDao {
 	private FesDBCP db;
@@ -32,6 +33,56 @@ public class MemberDao {
 			rs = pstmt.executeQuery();
 			rs.next();
 			cnt = rs.getInt("cnt");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.close(rs);
+			db.close(pstmt);
+			db.close(con);
+		}
+		return cnt;
+	}
+	
+	// 로그인시 사용자 이름 가져오는 전담 처리함수
+	public String getName(String id) {
+		String name = null;
+		
+		con = db.getCon();
+		String sql = mSQL.getSQL(mSQL.SEL_NAME_INFO);
+		pstmt = db.getPSTMT(con, sql);
+		try {
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			rs.next();
+			name = rs.getString("user_name");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.close(rs);
+			db.close(pstmt);
+			db.close(con);
+		}
+		return name;
+	}
+	
+	// 회원가입 전담 처리함수
+	public int addMember(MemberVO mVO) {
+		int cnt = 0;
+		
+		con = db.getCon();
+		String sql = mSQL.getSQL(mSQL.ADD_MEMBER);
+		pstmt = db.getPSTMT(con, sql);
+		try {
+			pstmt.setString(1, mVO.getName());
+			pstmt.setString(2, mVO.getId());
+			pstmt.setString(3, mVO.getPw());
+			pstmt.setString(4, mVO.getBirth());
+			pstmt.setString(5, mVO.getTel());
+			pstmt.setString(6, mVO.getMail());
+			pstmt.setString(7, mVO.getAdd());
+			pstmt.setString(8, mVO.getGen());
+			
+			cnt = pstmt.executeUpdate();
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
--- FestivalProject/src/main/webapp/resources/js/tmddus.js
+++ FestivalProject/src/main/webapp/resources/js/tmddus.js
@@ -16,4 +16,31 @@ $(document).ready(function() {
 	$('#logoutbtn').click(function() {
 		$(location).attr('href', '/festival/member/logoutProc.fes');
 	});
+	$('#backbtn').click(function() {
+		window.history.back();
+	});
+	$('#jbtn').click(function() {
+		var el = $('.lab');
+		for(var i=0; i<el.length; i++) {
+			if(!(el.eq(i).val())) {
+				el.eq(i).focus();
+				return;
+			}
+		}
+		if(!($('input:radio[name=gen]').is(':checked'))) {
+			alert('성별을 체크해주세요');
+			return;
+		}
+		var namepat = /^{0, 10}$/;
+		var idpat = /^[a-zA-Z0-9]{5,20}$/;
+		var pwpat = /^[a-zA-Z0-9]{5,20}$/;
+		
+		var test = $('#birth').val();
+		alert(test);
+		
+		
+		// 정규표현식 검사
+		$('#frm').attr('action', '/festival/member/joinProc.fes');
+		$('#frm').submit();
+	});
 });
